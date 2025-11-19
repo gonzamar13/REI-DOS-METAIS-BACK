7280f668887c4acc3e971e0fea28ebc7e8ce95e2
gonzamar13 <marcosgonzs13@gmail.com>
2025-11-19 20:25:19 -0300

    Se agrega variables de entorno

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine = create_engine("postgresql+psycopg2://postgres:reidosmetais2025@109.199.112.8:5433/postgres", echo=True)
+engine = create_engine("DATABASE_URL", echo=True)
 
 
 SessionLocal = sessionmaker(autocommit=False, bind=engine)
